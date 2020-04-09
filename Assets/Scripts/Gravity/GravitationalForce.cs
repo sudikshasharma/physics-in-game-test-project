1497760c2c6f2a5c22046f7b4b43550ef29e3b7e
Sudiksha <sudiksha.sd1@gmail.com>
2020-04-10 04:30:00 +0530

    Implemented Gravitation between objects

---
+++ Assets/Scripts/Gravity/GravitationalForce.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitationalForce : MonoBehaviour
+{
+    private GravitationalForce[] objects;
+    private LineRenderer lineR;
+    private Vector3 objSpeed;
+    private Vector3 acc;
+    private float bigG = 6.673e-11f;
+    public Vector3 totalForce;
+    public float objectMass;
+
+    void Start()
+    {
+        objects = GameObject.FindObjectsOfType<GravitationalForce>();
+        lineR = gameObject.AddComponent<LineRenderer>();
+        lineR.useWorldSpace = false;
+    }
+
+    void FixedUpdate()
+    {
+        CalculateNetForce();
+        transform.position += objSpeed;
+    }
+    void CalculateNetForce()
+    {
+        foreach (var objectA in objects)
+        {
+            int i = 1;
+            foreach (var objectB in objects)
+            {
+                if (objectA != objectB)
+                {
+                    Vector3 force = (objectA.transform.position - objectB.transform.position).normalized * (bigG * objectA.objectMass * objectB.objectMass / Mathf.Pow((objectA.transform.position - objectB.transform.position).magnitude, 2));
+                    RenderForceLines(force, i);
+                    AddForce(force);
+                    i += 2;
+                }
+            }
+        }
+    }
+
+    void RenderForceLines(Vector3 force, int i)
+    {
+        lineR.positionCount += i;
+        lineR.startWidth = 0.2f;
+        lineR.endWidth = 0.2f;
+        lineR.material = new Material(Shader.Find("Sprites/Default"));
+        lineR.material.color = Color.red;
+        lineR.SetPosition(i, Vector3.zero);
+        lineR.SetPosition(i + 1, force.normalized);
+    }
+
+    void AddForce(Vector3 force)
+    {
+        totalForce += force;
+        acc = totalForce / objectMass;
+        objSpeed += acc;
+    }
+}
